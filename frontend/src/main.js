ff7be080a41d879890b88d3b3a703549a62c36e5
awamej <a.wamej@gmail.com>
2023-05-25 19:39:55 +0200

    add bootstrap bootstrap-vue and sass

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from "bootstrap-vue";
+import "./assets/scss/custom.scss";
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 new Vue({
